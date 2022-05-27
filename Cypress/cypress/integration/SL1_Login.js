c7897a8397e725800c7e64bc220b81d391ce2cdd
cadumega <72901445+cadumega@users.noreply.github.com>
2022-05-27 20:34:54 -0300

    create test SenseData e documentacoes

---
+++ Cypress/cypress/integration/SL1_Login.js
@@ -0,0 +1,62 @@
+/// <reference types="Cypress" />
+import { LoginPageclass } from '../page_objects/objects'
+
+let loginPage = new LoginPageclass()
+
+it('Acesso de login e logout', function () {
+    //Logar com dados mais privados que estão no page_objects
+    loginPage.navigate();
+    loginPage.loginAcess();
+
+    //Conferir se estou realmente na página de Produtos
+    cy.get('.title').should('contain', 'Products')
+    cy.get('[data-test="product_sort_container"]').should('be.visible')
+
+    // Filtrar
+    cy.wait(1000)
+    cy.get('[data-test="product_sort_container"]').select('Price (low to high)');
+
+    // Adicionar produtos
+    cy.get('#add-to-cart-sauce-labs-onesie').click()
+    cy.wait(1000)
+    cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click()
+
+    // Acessar carrinho
+    cy.get('.shopping_cart_link').click()
+
+    //Conferir se estou realmente do carrinho e revisar itens selecionados
+    cy.get('.title').should('contain', 'Your Cart')
+    cy.get('#item_2_title_link > .inventory_item_name').should('contain', 'Sauce Labs Onesie')
+    cy.get('#item_3_title_link > .inventory_item_name').should('contain', 'Test.allTheThings() T-Shirt (Red)')
+
+    // Finalizar Compra, com dados mais privados que estão no page_objets
+    cy.get('#checkout').click()
+
+    //forcei a aparecer o error, digitando só o primeiro campo obrigatório
+    cy.get('#first-name').type('dsadasd')
+    cy.get('#continue').click()
+    cy.get('[data-test="error"]').contains('Error: Last Name is required')
+    cy.wait(1000)
+
+    //correto
+    loginPage.checkoutInformation()
+
+    // Conferir se irá aparecer Payment.. Shipping.. Informations
+    cy.get('.summary_info > :nth-child(1)').should('contain', 'Payment')
+    cy.get('.summary_info > :nth-child(3)').should('contain', 'Shipping')
+
+    cy.get('#finish').click()
+
+    // Retornar página de conclusão e agradecimento
+    cy.get('.complete-header').should('contain', 'THANK YOU FOR YOUR ORDER')
+    cy.get('#back-to-products').should('contain', 'Back Home')
+    cy.wait(2000)
+    cy.get('#back-to-products').click()
+
+    // Observações :
+    // Back to home esta retornando a tela de produtos.
+    // Tempo de execução 08.55 , daria para diminuir para 03.40 sem causar erro tirar os wait, coloquei só para demonstrar o teste rodando para algum usuário. 
+    // O Robot Framework com Selenium, costuma não acompanhar o tempo de execução do teste e se perde algumas vezes, o Cypress achei com essa performance melhor. Vantagens e desvantagens em cada ferramenta
+    .
+
+})
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # senseDataTeste
- testesQA
+
+1 - Criação de repositórios
+2 - Criação das pastas
+  2.1 - Configurações e instalações de dependências no Cypress
+
+    2.2 - Irei instalar o cypress localmente na pasta do projeto, executar terminal dentro da pasta.
+    // npm init -y.         //  (Irá criar uma file package.json para configs)
+    // npm install cypress  //  (Irá instalar o cypress no projeto, alguns arqs de configs no node_modules)
+    // npx cypress open    (irá carregar para abrir e irá criar a pasta do projeto)
+
+3 - Dentre as pastas criadas que são 4, iremos apagar as subpastas de exemplos getting-started e advanced-examples.
+
+4 - Irei criar uma pasta page_objects, afim de facilitar a criação de objetos com métodos que irei repetir nos testes.
+   4.1 - Vou aproveitar para criar um método que irá ter a função de logar o username e password. Crio o arquivo object.js
+   4.2 - Na pasta integration que crio o meu primeiro teste SL1_Login.js, importei o arquivo object.js para realizar a chamada corretamente nessa pasta, onde posso colocar variáveis ou outras funções para simplificar o teste.
+   4.3 - Como tem os atributos únicos de id , foi mais fácil de realizar essa seleção de elementos.
+   4.3 - Quero ver se esta executando tudo certo na ferramenta. Para rodar os testes executo no terminal npx cypress run.
+
+5- Obtive uns bugs que precisava executar os seguintes comandos:
+//npm i -D cy-verify-downloads
+//npm install cypress-downloadfile
+
+6- Executo novamente npx cypress run
+7 - Deu um erro de segurança do navegador no qual tive que colocar algumas configurações no json. "chromeWebSecurity": false
+8 - Primeiro teste de LOGIN OK!
+
+9 - Para agilizar o processo irei utilizar uma ferramenta de target(Open Select Playground) de elemento da própria ferramenta, para identificar sem precisar inspecionar o elemento a todo momento, basta eu clicar nela e apontar o elemento que quero, retorando para mim o atributo do elemento.
+    // 9.1 - verificar teste SL1_Login.js e page_objects login_page.js
+
+10 - Parei a execução no terminal (ctrl c) se quiser executar novamente só digitar npx cypress run e irá executar pelo terminal aparecendo se passou ou não. Caso execute querendo abrir a interface do programa ,digite npx cypress open.
+
